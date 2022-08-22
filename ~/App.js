258b37938fb2c1c9dbc3671280964c5fcafd46d7
Hansana Ranaweera <73303406+ranaHP@users.noreply.github.com>
2022-08-22 18:22:14 +0530

    This commit for remove unneccesary file

--- ~/App.js
+++
@@ -1,8 +0,0 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { DataProvider } from './src/hooks';
-import AppNavigation from './src/navigation/App';
-export default function App() {
-    return (React.createElement(DataProvider, null,
-        React.createElement(AppNavigation, null)));
-}
